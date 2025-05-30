ad8b5c498138936bcb8dde2aecd6753d6034f48c
efbrace <ebenezerflintwoodbrace@gmail.com>
2025-05-30 14:55:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const displayData = (data) => {
     productCard.innerHTML = `
         <div class="image-container">
           <img
-            src=${item.image}
+            src=${item.imageURL}
             alt=""
           />
         </div>
